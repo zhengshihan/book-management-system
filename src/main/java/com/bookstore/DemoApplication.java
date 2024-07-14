4085475a47e877e2bc2daf6970e870da43472a6e
zhengshihan <116463650+zhengshihan@users.noreply.github.com>
2024-07-14 08:03:03 +0100

    Update DemoApplication.java

--- src/main/java/com/bookstore/DemoApplication.java
+++ src/main/java/com/bookstore/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan("com.bookstore.model")
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
